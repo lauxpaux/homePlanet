e87b72b6b1d7465950be299895d513c685374055
Laura C Paulino <40530704+lauxpaux@users.noreply.github.com>
2020-07-27 09:06:44 -0400

    first commit
    header code and animation

---
+++ home-planet/final-code/home-planet.js
@@ -0,0 +1,25 @@
+const eyesTimeline = gsap.timeline({
+    repeat: -1
+
+})
+
+const eyeballs = document.querySelectorAll("path#ball, path#ball_2, path#ball_3, path#ball_4, path#ball_5, path#ball_6")
+
+eyesTimeline
+    .set(eyeballs, {y : 0})
+    .to(eyeballs, { y: 7, duration: 0.25, delay: 2, stagger: 0.25})
+    .to(eyeballs, { y: 0, duration: 0.25, delay: 4 })
+
+
+const hatTimeline = gsap.timeline({
+        repeat: -1, 
+        repeatDelay: 4
+    })
+
+const hat = document.querySelector("g#hat")
+
+hatTimeline
+    .set(hat, {y : 0})
+    .to(hat, { y: -50, rotation: -10, duration: 0.25, delay: 1})
+    .to(hat, { y: 0, rotation: 0, duration: 0.5, delay: 0.1})
+    
