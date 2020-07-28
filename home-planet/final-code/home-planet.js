3829d565b5a70b4197e7bada0e1c8393c42a4382
Laura C Paulino <40530704+lauxpaux@users.noreply.github.com>
2020-07-28 08:40:07 -0400

    second commit
    added home page with about and blog section.

--- home-planet/final-code/home-planet.js
+++ home-planet/final-code/home-planet.js
@@ -23,3 +23,44 @@ hatTimeline
     .to(hat, { y: -50, rotation: -10, duration: 0.25, delay: 1})
     .to(hat, { y: 0, rotation: 0, duration: 0.5, delay: 0.1})
     
+
+const leftArmTimeline = gsap.timeline({
+    repeat: -1 
+
+})
+
+const leftArm = document.querySelector('#left-arm')
+
+leftArmTimeline
+    .set(leftArm, {rotation: 0})
+    .to(leftArm, {rotation: 20, duration: 0.25, delay: 2})
+    .to(leftArm, {rotation: 0, duration: 0.25, delay: 2})
+
+
+    const rightArmTimeline = gsap.timeline({
+        repeat: -1 
+    
+    })
+    
+    const rightArm = document.querySelector('#right-arm')
+    
+    rightArmTimeline
+        .set(rightArm, {rotation: 0})
+        .to(rightArm, {rotation: -20, duration: 0.25, delay: 3})
+        .to(rightArm, {rotation: 0, duration: 0.25, delay: 2})
+
+
+const tvTimeline = gsap.timeline({
+    repeat: -1
+}) 
+
+const tvLight = document.querySelector('#tv-light')
+
+let o = 0.75
+
+tvTimeline 
+    .set(tvLight, {opacity: o, })
+    .to(tvLight, {opacity: 1, duration: 1, delay: 0.5})
+    .to(tvLight, {opacity: o})
+    .to(tvLight, {opacity: 1, duration: 0.4, delay: 0.5})
+    .to(tvLight, {opacity: o})
